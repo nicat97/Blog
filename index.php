93e499e095af9f68a14ea089cfd2731a6f8d8db7
Nijat Mammadov <n1c4t97@gmail.com>
2017-02-23 21:33:33 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 session_start();
+ob_start();
+
+//SSL sertifikatı varsa əgər istifadə oluna bilər
+/*if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
+    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $redirect);
+    exit();
+}*/
 
 include 'app/Bootstrap.php';
 include 'app/Controller.php';
@@ -11,4 +20,5 @@ $ROUTE = $Boot->init($_GET['route']);
 $controller = new Controller();
 $controller->detectRoute($ROUTE);
 
+ob_end_flush();
 ?>
