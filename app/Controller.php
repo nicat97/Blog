8f3410b9ff0536d32de7f5cb34e63eb439951c21
Nijat Mammadov <n1c4t97@gmail.com>
2017-02-23 22:16:41 +0400

    Update Controller.php

--- app/Controller.php
+++ app/Controller.php
@@ -2,19 +2,17 @@
 
 class Controller{
 
-    public function detectRoute(array $ROUTE){
+    public function detectRoute($status, array $ROUTE){
 
-        $fileName = __DIR__ . "/pages/" . $ROUTE[0] . ".php";
-
-        if(file_exists($fileName)){
+        if(!$status){
+            include "app/lib/404.php";
+        }else{
 
             include "pages/" . $ROUTE[0] . ".php";
 
             $ROUTE[1] = isset($ROUTE[1]) ? $ROUTE[1] : null; //Əgər massivin 1 nömrəli (ikinci) elementi yoxdursa xəta verməsin.
-            $callClass = new $ROUTE[0]($ROUTE[1]); //Axtarılan səhifənin sinifini çağırır
+            $callClass = new $ROUTE[0]($ROUTE[1]); //_construct metoduna parametr verir
 
-        }else{
-            include "app/lib/404.php";
         }
     }
 
