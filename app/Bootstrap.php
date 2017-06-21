bd139856ba89b012a0d87212c41610565d80874f
Nijat Mammadov <n1c4t97@gmail.com>
2017-06-21 20:56:43 +0400

    Update Bootstrap.php

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -4,7 +4,7 @@ class Bootstrap{
     public function init($ROUTE){
 
         $ROUTE = str_replace("I", "ı", $ROUTE);
-        //PHP I hərfini kiçildəndə İngilis böyük i (I) ilə qatışdırır və nəticədə i qaytarır.
+        //PHP I hərfini kiçildəndə İngilis böyük i (I) ilə qarışdırır və nəticədə i qaytarır.
         $ROUTE = mb_strtolower($ROUTE, "UTF-8");
         //strtolower() metodu yalnız ASCII cədvəlindəki hərfləri kiçildir. mb_convert_case($string, MB_CASE_LOWER, "UTF-8")
         //metodu da yuxarıdakı ilə eyni işi görür. Əlavə məlumat: http://php.net/manual/en/function.strtolower.php
